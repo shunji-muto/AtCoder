2a91085c7f4089979e370362fd64b29c9d2c835d
shunji-muto <sit.shunji.muto@gmail.com>
2025-01-06 18:44:09 +0900

    add 387

---
+++ old/Problem387/c.cpp
@@ -0,0 +1,30 @@
+// #include <iostream>
+// #include <math.h>
+// #include <bits/stdc++.h>
+
+
+// using namespace std;
+
+// long long Calc(long long n){
+//     vector<int> num;
+//     for(;n;n/10){
+//         num.push_back(n%10);
+//     }
+//     reverse(num.begin(), num.end());
+
+//     int len = num.size();
+//     long long res = 0;
+//     bool flag = 0;
+
+//     for(int i = 1; i < len; i++){
+//         res += min(num[0], num[i])
+//     }
+// }
+
+// int main()
+// {
+//     long long L, R;
+// 	cin >> L >> R;
+
+//     cout << result << endl;
+// }
